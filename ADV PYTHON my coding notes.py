a5704b9e2d09e90e93dfd6d9b5ce75d935d72b89
Vishal <vmahire5@gmail.com>
2022-11-16 14:03:32 +0530

    new database file with new data base file

--- ADV PYTHON my coding notes.py
+++ ADV PYTHON my coding notes.py
@@ -22,3 +22,62 @@ L2 = Laptop('DELL','Pro',80000)
 print(L1.brand_name,L1.model_name,L1.price)
 print(L2.brand_name,L2.model_name,L2.price)
 
+
+
+
+
+
+
+
+
+DATABASES
+
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "mssql",
+        "NAME": "python34",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "DESKTOP-QPEDHM8",
+        "PORT": "",
+        "OPTIONS": {"driver": "ODBC Driver 17 for SQL Server",
+        },
+    },
+}
+
+
+
+
+
+
+
+
+MODELS
+
+
+from django.db import models
+
+# Create your models here.
+class prd_details(models.Model):
+    name = models.CharField(max_length=20)
+    prd_qnty = models.IntegerField()
+    prd_price = models.FloatField()
+    prd_loc = models.CharField(max_length=10)
+
+
+
+
+ADMINS
+
+admin.py
+
+
+from django.contrib import admin
+from .models import prd_details
+# Register your models here.
+class prd_detailsAdmin(admin.ModelAdmin):
+    list_display = ['name','prd_qnty','prd_price','prd_loc']
+
+admin.site.register(prd_details,prd_detailsAdmin)
